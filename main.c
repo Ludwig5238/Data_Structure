2a20afde799f2b4bb409815227da03df054b20c4
Ludwig5238 <Ludwig5238@outlook.com>
2025-12-08 17:10:20 +0800

    20251208-1

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello, from Data_Structure!\n");
+enum states
+{
+    FALSE = 0,
+    ERROR = 0,
+    TRUE = 1,
+    OK = 1
+};
+
+int main()
+{
+    char status = OK;
+    if (status == TRUE)
+    {
+        printf("Status is TRUE\n");
+    }
+    else
+    {
+        printf("Status is FALSE\n");
+    }
+    return 0;
 }
