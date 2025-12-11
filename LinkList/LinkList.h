e2d2789392e1b96052d5a16917cced9be5df5091
Ludwig5238 <Ludwig5238@outlook.com>
2025-12-11 21:17:25 +0800

    20251211-end

--- LinkList/LinkList.h
+++ LinkList/LinkList.h
@@ -19,9 +19,23 @@ typedef struct LNode
     struct LNode *next;
 } LNode, *LinkList;
 
-// 初始化带头节点的单链表L
+// 初始化单链表
 Status InitList(LinkList *L);
-
-
+// 销毁单链表L
+Status DestroyList(LinkList *L);
+// 构建单链表L
+Status CreateList(LinkList *L);
+// 获取单链表L中的第i个元素
+Status GetElem(LinkList L, int i, ElemType *e);
+// 获取单链表L的长度
+int ListLength(LinkList L);
+// 查找线性表中值为e的元素
+Status LocateElem(LinkList L, ElemType e, int *i);
+// 删除单链表L中的第i个元素
+Status DeleteElem(LinkList *L, int i);
+// 插入元素e到单链表L的第i个位置
+Status InsertElem(LinkList *L, int i, ElemType e);
+// 将单链表中的元素从小到大排序
+Status SortList(LinkList *L);
 
 #endif
--- main.c
+++ main.c
@@ -4,10 +4,60 @@
 int main()
 {
     LinkList L;
-    Status status = InitList(&L);
-    if (status == OK)
+    if (InitList(&L) == OK)
+        printf("单链表初始化成功！\n");
+    else
+        printf("单链表初始化失败！\n");
+
+    if (CreateList(&L) == OK)
     {
-        printf("初始化成功\n");
+        printf("单链表创建成功！\n");
+        ElemType e;
+        int length = ListLength(L);
+        for (int i = 1; i <= length; i++)
+        {
+            if (GetElem(L, i, &e) == OK)
+                printf("单链表的第 %d 个元素是: %d\n", i, e);
+            else
+                printf("获取第 %d 个元素失败！\n", i);
+        }
     }
+    else
+        printf("单链表创建失败！\n");
+
+    if (SortList(&L) == OK)
+        printf("单链表排序成功！\n");
+    else
+        printf("单链表排序失败！\n");
+    // 遍历链表并打印元素
+    printf("排序后的单链表元素为:\n");
+    ElemType e;
+    int length = ListLength(L);
+    for (int i = 1; i <= length; i++)
+    {
+        if (GetElem(L, i, &e) == OK)
+            printf("%d ", e);
+        else
+            printf("获取第 %d 个元素失败！\n", i);
+    }
+    printf("\n");
+    ElemType searchElem = 50; // 假设要查找的元素值
+    int index;
+    if (LocateElem(L, searchElem, &index) == OK)
+        printf("元素 %d 在单链表中的位置是: %d\n", searchElem, index);
+    else
+        printf("元素 %d 不在单链表中！\n", searchElem);
+
+    if (DestroyList(&L) == OK)
+        printf("单链表销毁成功！\n");
+    else
+        printf("单链表销毁失败！\n");
+
     return 0;
 }
+
+// 顺序线性表
+// 单链表
+// 循环链表
+// 双向链表
+// 双向循环链表
