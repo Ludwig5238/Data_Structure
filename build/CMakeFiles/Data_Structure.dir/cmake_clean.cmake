52e9b6c590e25bcf06489987d19506dec7063b86
Ludwig5238 <Ludwig5238@outlook.com>
2025-12-16 22:10:41 +0800

    20251216-2

---
+++ build/CMakeFiles/Data_Structure.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Data_Structure.dir/Stack/LinkStack.c.o"
+  "CMakeFiles/Data_Structure.dir/Stack/LinkStack.c.o.d"
+  "CMakeFiles/Data_Structure.dir/main.c.o"
+  "CMakeFiles/Data_Structure.dir/main.c.o.d"
+  "Data_Structure"
+  "Data_Structure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Data_Structure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
